a51cf7649069c80bbb0646cee3a66c5a75e5ed62
Sungwon Choi <48509269+zhtmr@users.noreply.github.com>
2023-12-01 09:36:58 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 208 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    String[] arr = {">", "<", "=="};
+
+    Scanner scanner = new Scanner(System.in);
+    int a = scanner.nextInt();
+    int b = scanner.nextInt();
+
+    if (a > b) {
+      System.out.println(">");
+    } else if (a < b) {
+      System.out.println("<");
+    } else {
+      System.out.println("==");
+    }
+
+    scanner.close();
+  }
+}
