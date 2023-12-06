d45f6c250b3a3694f6ceaf2e3bfb0149331a6c55
Sungwon Choi <48509269+zhtmr@users.noreply.github.com>
2023-12-06 11:41:06 +0900

    [Bronze V] Title: 윤년, Time: 212 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int input = scanner.nextInt();
+
+    if (input % 4 == 0 && input % 100 != 0 || input % 400 == 0) {
+      System.out.println(1);
+    } else {
+      System.out.println(0);
+    }
+  }
+
+}
