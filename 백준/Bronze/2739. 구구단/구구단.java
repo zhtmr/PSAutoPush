1e796ace9abe6ddf4eab7a38d4c889aa8dc94ce2
Sungwon Choi <48509269+zhtmr@users.noreply.github.com>
2023-12-07 20:34:32 +0900

    [Bronze V] Title: 구구단, Time: 144 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int input = Integer.parseInt(br.readLine());
+    for (int i = 1; i < 10; i++) {
+      System.out.printf("%d * %d = %d\n", input, i, input * i);
+    }
+
+  }
+
+}
