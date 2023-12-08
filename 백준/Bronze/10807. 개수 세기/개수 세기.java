41edf268c2346e7fb6b67ba918e7df68b621be2f
c.sw <48509269+zhtmr@users.noreply.github.com>
2023-12-08 21:46:27 +0900

    [Bronze V] Title: 개수 세기, Time: 128 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    StringBuilder sb = new StringBuilder();
+
+    int N = Integer.parseInt(st.nextToken());
+    ArrayList<Integer> arr = new ArrayList<>();
+    int count = 0;
+
+    StringTokenizer numbers = new StringTokenizer(br.readLine());
+    while (numbers.hasMoreElements()){
+      arr.add(Integer.parseInt(numbers.nextToken()));
+    }
+
+    int v = Integer.parseInt(br.readLine());
+
+    for (int j : arr) {
+      if (j == v) {
+        count++;
+      }
+    }
+
+    bw.write(String.valueOf(count));
+    bw.flush();
+    bw.close();
+  }
+
+}
