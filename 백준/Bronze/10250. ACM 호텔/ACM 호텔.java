024afe626dd33e133b4b36a62803e9b77910020e
c.sw <48509269+zhtmr@users.noreply.github.com>
2023-12-17 15:43:00 +0900

    [Bronze III] Title: ACM 호텔, Time: 128 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -0,0 +1,80 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  static FastReader scan = new FastReader();
+  //정답은 sb에 append 를 사용하여 출력
+  //만약 개행까지 출력하고 싶으면 append('\n')을 추가
+  static StringBuilder sb = new StringBuilder();
+
+  public static void main(String[] args) {
+    input();
+  }
+
+  static void input() {
+    int t = scan.nextInt();
+    for (int i = 0; i < t; i++) {
+      int h = scan.nextInt();
+      int w = scan.nextInt();
+      int n = scan.nextInt();
+
+      sb.append((n % h == 0) ? h * 100 + n / h : n % h * 100 + n / h + 1).append('\n');
+    }
+    System.out.println(sb);
+  }
+
+  static class FastReader {
+
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+      br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public FastReader(String s) throws FileNotFoundException {
+      br = new BufferedReader(new FileReader(new File(s)));
+    }
+
+    String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+      String str = "";
+      try {
+        str = br.readLine();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      return str;
+    }
+  }
+
+
+}
