058e4e90a10eb658c77a242b302a4a3441f1e98d
csw <48509269+zhtmr@users.noreply.github.com>
2023-12-28 21:35:55 +0900

    [Bronze IV] Title: 민주주의, Time: 124 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/30999. 민주주의/민주주의.java
@@ -0,0 +1,88 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  static FastReader scan = new FastReader();
+  static StringBuilder sb = new StringBuilder();
+
+  public static void main(String[] args) {
+    input();
+  }
+
+  static void input() {
+    int N = scan.nextInt();
+    int M = scan.nextInt();
+    int result = 0;
+
+    for (int i = 0; i < N; i++) {
+      int count = 0;
+      String s = scan.next();
+      for (int j = 0; j < M; j++) {
+        if (s.charAt(j) == 'O') {
+          count++;
+        }
+      }
+      if (count > M >> 1) {
+        result++;
+      }
+    }
+
+    System.out.println(result);
+
+  }
+
+  static class FastReader {
+
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+      br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public FastReader(String s) throws FileNotFoundException {
+      br = new BufferedReader(new FileReader(new File(s)));
+    }
+
+    String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+      String str = "";
+      try {
+        str = br.readLine();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      return str;
+    }
+  }
+
+
+}
