c68e9aba3e796628e1045302a4e3f8271f7b78aa
csw <48509269+zhtmr@users.noreply.github.com>
2024-01-02 09:15:30 +0900

    [Bronze V] Title: We love kriii, Time: 116 ms, Memory: 13992 KB -BaekjoonHub

--- 백준/Bronze/10718. We love kriii/We love kriii.java
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -1,6 +1,74 @@
-public class Main{
-    public static void main(String[] args){
-        System.out.println("강한친구 대한육군");
-        System.out.println("강한친구 대한육군");
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  static FastReader scan = new FastReader();
+  static StringBuilder sb = new StringBuilder();
+
+  public static void main(String[] args) {
+    input();
+  }
+
+  static void input() {
+    System.out.println("강한친구 대한육군");
+    System.out.println("강한친구 대한육군");
+  }
+
+  static class FastReader {
+
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+      br = new BufferedReader(new InputStreamReader(System.in));
     }
+
+    public FastReader(String s) throws FileNotFoundException {
+      br = new BufferedReader(new FileReader(new File(s)));
+    }
+
+    String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+      String str = "";
+      try {
+        str = br.readLine();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      return str;
+    }
+  }
+
+
 }
+
