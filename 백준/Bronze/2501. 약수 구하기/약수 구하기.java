03bd912b042139e4cf566919bb24ca8a1d1158cc
csw <48509269+zhtmr@users.noreply.github.com>
2024-01-09 14:25:52 +0900

    [Bronze III] Title: 약수 구하기, Time: 128 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  static FastReader scan = new FastReader();
+  static StringBuilder sb = new StringBuilder();
+
+  public static void main(String[] args) {
+    input();
+  }
+
+  static void input() {
+    int N = scan.nextInt();
+    int K = scan.nextInt();
+    int cnt = 0;
+
+    for (int i = 1; i <= N; i++) {
+      if (N % i == 0) {
+        sb.append(i).append(",");
+        cnt++;
+      }
+    }
+
+    int[] arr = new int[cnt];
+    String[] split = sb.toString().split(",");
+    for (int i = 0; i < split.length; i++) {
+      arr[i] = Integer.parseInt(split[i].trim());
+    }
+
+    System.out.println(arr.length < K ? 0 : arr[K-1]);
+  }
+
+  static class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+      br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public FastReader(String s) throws FileNotFoundException {
+      br = new BufferedReader(new FileReader(new File(s)));
+    }
+
+    String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+      String str = "";
+      try {
+        str = br.readLine();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      return str;
+    }
+  }
+
+}
