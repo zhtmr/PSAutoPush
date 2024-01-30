d210e73c2919e96e2054683c636daf8c1e09b9e7
csw <48509269+zhtmr@users.noreply.github.com>
2024-01-30 20:25:42 +0900

    [Bronze II] Title: 블랙잭, Time: 124 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  static FastReader scan = new FastReader();
+  static StringBuilder sb = new StringBuilder();
+
+  public static void main(String[] args) {
+    input();
+  }
+
+  static void input() {
+    int n = scan.nextInt();
+    int m = scan.nextInt();
+    int[] arr = new int[n];
+    int sum, max = 0;
+
+    for (int i = 0; i < n; i++) {
+      arr[i] = scan.nextInt();
+    }
+
+
+    loop:
+    for (int i = 0; i < n; i++) {
+      for (int j = i + 1; j < n; j++) {
+        for (int k = j + 1; k < n; k++) {
+          sum = arr[i] + arr[j] + arr[k];
+          if (sum == m) {
+            max = sum;
+            break loop;
+          }
+          if (sum > max && sum < m) {
+            max = sum;
+          }
+        }
+      }
+    }
+
+    System.out.println(max);
+  }
+
+  static class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+      br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public FastReader(String s) throws FileNotFoundException {
+      br = new BufferedReader(new FileReader(new File(s)));
+    }
+
+    String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+      String str = "";
+      try {
+        str = br.readLine();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      return str;
+    }
+  }
+
+}
