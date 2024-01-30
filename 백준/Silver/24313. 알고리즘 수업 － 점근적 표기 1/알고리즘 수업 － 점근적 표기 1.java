e332be9d50b0a523dddd4d366be336aebb187ec1
csw <48509269+zhtmr@users.noreply.github.com>
2024-01-30 18:29:54 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 124 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  static FastReader scan = new FastReader();
+  static StringBuilder sb = new StringBuilder();
+
+  public static void main(String[] args) {
+    input();
+  }
+
+  static void input() {
+    int a1 = scan.nextInt();
+    int a0 = scan.nextInt();
+    int c = scan.nextInt();
+    int n0 = scan.nextInt();
+    if (a1 * n0 + a0 <= c * n0 && a1 <= c) {
+      System.out.println(1);
+    } else {
+      System.out.println(0);
+    }
+
+  }
+
+  static class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+      br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public FastReader(String s) throws FileNotFoundException {
+      br = new BufferedReader(new FileReader(new File(s)));
+    }
+
+    String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+      String str = "";
+      try {
+        str = br.readLine();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      return str;
+    }
+  }
+
+}
