7d6d85d1e870ebbce561e9d5e50f4e1c0761f4bd
csw <48509269+zhtmr@users.noreply.github.com>
2024-05-29 16:21:56 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 128 ms, Memory: 14612 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -0,0 +1,123 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  static FastReader scan = new FastReader();
+  static StringBuilder sb = new StringBuilder();
+
+  public static void main(String[] args) {
+    input();
+  }
+
+  static void input() {
+    int M = scan.nextInt();
+    int N = scan.nextInt();
+
+    char[][] board = new char[M][N];
+    for (int i = 0; i < M; i++) {
+      board[i] = scan.nextLine().toCharArray();
+    }
+
+    System.out.println(result(M, N, board));
+
+  }
+
+
+  private static int result(int m, int n,char[][] board) {
+    char[][] chess1 = {
+        {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+        {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+        {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+        {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'}
+    };
+
+    char[][] chess2 = {
+        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+        {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+        {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+        {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+        {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'}
+    };
+
+    int min = Integer.MAX_VALUE;
+
+    for (int i = 0; i < m - 7; i++) {
+      for (int j = 0; j < n - 7; j++) {
+        int c1 = countRepaint(i, j, chess1, board);
+        int c2 = countRepaint(i, j, chess2, board);
+        min = Math.min(c1, min);
+        min = Math.min(c2, min);
+      }
+    }
+
+    return min;
+
+  }
+
+  private static int countRepaint(int i, int j, char[][] chess, char[][] board) {
+    int count = 0;
+    for (int k = 0; k < 8; k++) {
+      for (int l = 0; l < 8; l++) {
+        if (chess[k][l] != board[i + k][j + l]) {
+          count++;
+        }
+      }
+    }
+    return count;
+  }
+
+  static class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+      br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public FastReader(String s) throws FileNotFoundException {
+      br = new BufferedReader(new FileReader(new File(s)));
+    }
+
+    String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+      String str = "";
+      try {
+        str = br.readLine();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      return str;
+    }
+  }
+
+}
