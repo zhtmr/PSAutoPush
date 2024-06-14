6ecb6ad2d2eb039e39c40a1e868cf3aee33fbdda
Sungwon Choi <48509269+zhtmr@users.noreply.github.com>
2024-06-14 20:50:35 +0900

    [Silver IV] Title: 설탕 배달, Time: 116 ms, Memory: 14400 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -13,44 +13,24 @@ public class Main {
   static void input() {
     int N = scan.nextInt();
     int[] dp = new int[5001];
+
+    for (int i = 0; i <= 5000; i++) {
+      dp[i] = -1;
+    }
+
     dp[3] = 1;
-    dp[4] = -1;
     dp[5] = 1;
-    dp[6] = 2;
-    dp[7] = -1;
-//    dp[8] = 2;
-//    dp[9] = 3;
-//    dp[10] = 2;
-//    dp[11] = 3;
-//    dp[12] = 4;
-//    dp[13] = 3;
-//    dp[14] = 4;
-//    dp[15] = 3;
-//    dp[16] = 4;
-//    dp[17] = 5;
-//    dp[18] = 4;
-//    dp[19] = 5;
-//    dp[20] = 4;
 
-
-
-    for (int i = 8; i < dp.length; i++) {
-      if (dp[i - 3] == -1) {
-        dp[i] = dp[i - 5] + 1;
-      } else if (dp[i - 5] == -1) {
+    for (int i = 6; i <= N; i++) {
+      if (dp[i - 3] != -1) {
         dp[i] = dp[i - 3] + 1;
-      } else {
-        dp[i] = Math.min(dp[i - 3] + 1, dp[i - 5] + 1);
+      }
+      if (dp[i - 5] != -1) {
+        dp[i] = dp[i] == -1 ? dp[i - 5] + 1 : Math.min(dp[i], dp[i - 5] + 1);
       }
     }
 
-//    for (int i = 3; i < N; i++) {
-//      System.out.println("i = " + i + " " + dp[i]);
-//    }
-
     System.out.println(dp[N]);
-
-
   }
 
   static class FastReader {
@@ -98,5 +78,4 @@ public class Main {
       return str;
     }
   }
-
 }
