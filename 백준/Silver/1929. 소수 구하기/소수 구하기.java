7accaca96486be2f4051be04b875d1e772868c6a
Sungwon Choi <48509269+zhtmr@users.noreply.github.com>
2024-06-26 20:22:35 +0900

    [Silver III] Title: 소수 구하기, Time: 236 ms, Memory: 20548 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,79 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  static FastReader scan = new FastReader();
+  static StringBuilder sb = new StringBuilder();
+
+  public static void main(String[] args) {
+    input();
+  }
+
+  static void input() {
+    int M = scan.nextInt();
+    int N = scan.nextInt();
+    boolean[] arr = new boolean[N + 1];
+    arr[0] = arr[1] = true;
+    for (int i = 2; i * i <= N; i++) {
+      if (!arr[i]) {
+        for (int j = i * i; j <= N; j += i) {
+          arr[j] = true;
+        }
+      }
+    }
+    for (int i = M; i <= N; i++) {
+      if (!arr[i]) {
+        sb.append(i).append("\n");
+      }
+    }
+    System.out.println(sb);
+  }
+
+  static class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+      br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public FastReader(String s) throws FileNotFoundException {
+      br = new BufferedReader(new FileReader(new File(s)));
+    }
+
+    String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+      String str = "";
+      try {
+        str = br.readLine();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      return str;
+    }
+  }
+
+}
