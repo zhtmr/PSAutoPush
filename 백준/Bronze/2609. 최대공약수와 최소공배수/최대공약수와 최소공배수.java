3aee93a941d3d8f7b4338ce65b632a8942639fde
Sungwon Choi <48509269+zhtmr@users.noreply.github.com>
2024-06-29 15:37:26 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 100 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int a = Integer.parseInt(st.nextToken());
+    int b = Integer.parseInt(st.nextToken());
+
+    int gcd = GCD(a, b);
+    int lcm = a * b / gcd;
+    System.out.println(gcd);
+    System.out.println(lcm);
+  }
+
+  private static int GCD(int a, int b) {
+    if (b == 0) {
+      return a;
+    }
+    return GCD(b, a % b);
+  }
+}
