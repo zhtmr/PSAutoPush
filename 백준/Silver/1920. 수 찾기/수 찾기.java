6c8be1f27afdb4c50b57bb29b08fb6e606f61014
Sungwon Choi <48509269+zhtmr@users.noreply.github.com>
2024-07-07 17:28:46 +0900

    [Silver IV] Title: 수 찾기, Time: 1688 ms, Memory: 63012 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  static FastReader scan = new FastReader();
+  static StringBuilder sb = new StringBuilder();
+
+  public static void main(String[] args) {
+    input();
+  }
+
+  static void input() {
+    int N = scan.nextInt();
+    HashSet<Integer> set = new HashSet<>();
+    for (int i = 0; i < N; i++) {
+      set.add(scan.nextInt());
+    }
+    int M = scan.nextInt();
+    for (int i = 0; i < M; i++) {
+      if (set.contains(scan.nextInt())) {
+        System.out.println(1);
+      } else {
+        System.out.println(0);
+      }
+    }
+  }
+
+  static class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+      br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public FastReader(String s) throws FileNotFoundException {
+      br = new BufferedReader(new FileReader(new File(s)));
+    }
+
+    String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+      String str = "";
+      try {
+        str = br.readLine();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      return str;
+    }
+  }
+
+}
