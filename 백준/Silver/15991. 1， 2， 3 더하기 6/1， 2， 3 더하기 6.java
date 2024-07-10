5687d36e6af7c52541e93198d969e735013907e0
Sungwon Choi <48509269+zhtmr@users.noreply.github.com>
2024-07-10 22:45:55 +0900

    [Silver I] Title: 1, 2, 3 더하기 6, Time: 132 ms, Memory: 15548 KB -BaekjoonHub

---
+++ 백준/Silver/15991. 1， 2， 3 더하기 6/1， 2， 3 더하기 6.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  static FastReader scan = new FastReader();
+  static StringBuilder sb = new StringBuilder();
+
+  public static void main(String[] args) {
+    input();
+  }
+
+  static void input() {
+    long[] dp = new long[100001];
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 2;
+    dp[4] = 3;
+    dp[5] = 3;
+    dp[6] = 6;
+    for (int i = 7; i < dp.length; i++) {
+      dp[i] = (dp[i - 2] + dp[i - 4] + dp[i - 6]) % 1_000_000_009;
+    }
+
+    int n = scan.nextInt();
+    for (int i = 1; i <= n; i++) {
+      sb.append(dp[scan.nextInt() % 1_000_000_009]).append("\n");
+    }
+    System.out.println(sb);
+  }
+
+  static class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+      br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public FastReader(String s) throws FileNotFoundException {
+      br = new BufferedReader(new FileReader(new File(s)));
+    }
+
+    String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+      String str = "";
+      try {
+        str = br.readLine();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      return str;
+    }
+  }
+
+}
