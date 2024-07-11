e8ddb82d86675f84387c63b4b7ae37d30765a442
Sungwon Choi <48509269+zhtmr@users.noreply.github.com>
2024-07-11 19:49:32 +0900

    [Silver I] Title: 카드 구매하기, Time: 128 ms, Memory: 14528 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -0,0 +1,75 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  static FastReader scan = new FastReader();
+  static StringBuilder sb = new StringBuilder();
+
+  public static void main(String[] args) {
+    input();
+  }
+
+  static void input() {
+    int N = scan.nextInt();
+    int[] P = new int[N + 1];
+    int[] dp = new int[N + 1];
+    for (int i = 1; i <= N; i++) {
+      P[i] = scan.nextInt();
+    }
+
+    for (int i = 1; i <= N; i++) {
+      for (int j = 1; j <= i; j++) {
+        dp[i] = Math.max(dp[i], dp[i - j] + P[j]);
+      }
+    }
+    System.out.println(dp[N]);
+  }
+
+  static class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+      br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public FastReader(String s) throws FileNotFoundException {
+      br = new BufferedReader(new FileReader(new File(s)));
+    }
+
+    String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+      String str = "";
+      try {
+        str = br.readLine();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      return str;
+    }
+  }
+
+}
