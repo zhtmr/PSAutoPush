3f15b3ecbd1c80997c2fee41abf1b90a48a57f08
Sungwon Choi <48509269+zhtmr@users.noreply.github.com>
2024-07-11 21:36:12 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1108 ms, Memory: 173948 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  static FastReader scan = new FastReader();
+  static StringBuilder sb = new StringBuilder();
+
+  public static void main(String[] args) {
+    input();
+  }
+
+  static void input() {
+    int N = scan.nextInt();
+    HashMap<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < N; i++) {
+      int key = scan.nextInt();
+      map.put(key, map.getOrDefault(key, 0) + 1);
+    }
+    int M = scan.nextInt();
+    for (int i = 0; i < M; i++) {
+      sb.append(map.getOrDefault(scan.nextInt(), 0)).append(" ");
+    }
+    System.out.println(sb);
+  }
+
+  static class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+      br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public FastReader(String s) throws FileNotFoundException {
+      br = new BufferedReader(new FileReader(new File(s)));
+    }
+
+    String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+      String str = "";
+      try {
+        str = br.readLine();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      return str;
+    }
+  }
+
+}
