2b33d5a55dbc4131b9af1bbbb9e25ac001667386
Sungwon Choi <48509269+zhtmr@users.noreply.github.com>
2024-07-22 17:26:22 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 368 ms, Memory: 71920 KB -BaekjoonHub

---
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
@@ -0,0 +1,95 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  static FastReader scan = new FastReader();
+  static StringBuilder sb = new StringBuilder();
+
+  public static void main(String[] args) {
+    input();
+  }
+
+  static void input() {
+    int T = scan.nextInt();
+    for (int i = 0; i < T; i++) {
+      int n = scan.nextInt();
+      boolean[] primeArr = sieve(n);
+      goldbachCnt(primeArr, n);
+    }
+    System.out.println(sb);
+  }
+
+  static boolean[] sieve(int a) {
+    boolean[] arr = new boolean[a + 1];
+    arr[0] = arr[1] = true;
+
+    for (int i = 2; i * i <= a; i++) {
+      if (!arr[i]) {
+        for (int j = i * i; j <= a; j += i) {
+          arr[j] = true;
+        }
+      }
+    }
+    return arr;
+  }
+
+  private static void goldbachCnt(boolean[] arr, int n) {
+    int count = 0;
+    for (int i = 2; i <= n / 2; i++) {
+      if (!arr[i] && !arr[n - i]) {
+        count++;
+      }
+    }
+    sb.append(count).append('\n');
+  }
+
+
+
+  static class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+      br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public FastReader(String s) throws FileNotFoundException {
+      br = new BufferedReader(new FileReader(new File(s)));
+    }
+
+    String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+      String str = "";
+      try {
+        str = br.readLine();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      return str;
+    }
+  }
+
+}
