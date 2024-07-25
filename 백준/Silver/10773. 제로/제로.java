f747b2540d6168e9fa88ea8e184c0e9609829835
Sungwon Choi <48509269+zhtmr@users.noreply.github.com>
2024-07-26 00:45:26 +0900

    [Silver IV] Title: 제로, Time: 280 ms, Memory: 29172 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  static FastReader scan = new FastReader();
+  static StringBuilder sb = new StringBuilder();
+
+  public static void main(String[] args) {
+    input();
+  }
+
+  static void input() {
+    ArrayDeque<Integer> stack = new ArrayDeque<>();
+    int K = scan.nextInt();
+    for (int i = 0; i < K; i++) {
+      int key = scan.nextInt();
+      if (key == 0) {
+        stack.removeLast();
+        continue;
+      }
+      stack.offer(key);
+    }
+    int sum = 0;
+    for (int e : stack) {
+      sum += e;
+    }
+    System.out.println(sum);
+  }
+
+  static class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+      br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public FastReader(String s) throws FileNotFoundException {
+      br = new BufferedReader(new FileReader(new File(s)));
+    }
+
+    String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+      String str = "";
+      try {
+        str = br.readLine();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      return str;
+    }
+  }
+
+}
