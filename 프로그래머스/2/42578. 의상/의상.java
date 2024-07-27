8b914d16c156a2da89749aafbe032d5d7a6b55d5
Sungwon Choi <48509269+zhtmr@users.noreply.github.com>
2024-07-27 19:22:15 +0900

    [level 2] Title: 의상, Time: 0.13 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < clothes.length; i++) {
+          map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+        }
+
+    
+        for (int count : map.values()) {
+          answer *= count + 1;
+        }
+        
+        return answer - 1;
+    }
+}
