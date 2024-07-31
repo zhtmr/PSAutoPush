2ca2074eb93df7865f013f8bc430cf48c8c9ab46
Sungwon Choi <48509269+zhtmr@users.noreply.github.com>
2024-07-31 20:15:29 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2600 ms, Memory: 363384 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -2,103 +2,81 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-  static FastReader scan = new FastReader();
-  static StringBuilder sb = new StringBuilder();
-
-  public static void main(String[] args) {
-    input();
-  }
-
-  static void input() {
-
-    int T = scan.nextInt();
-    for (int i = 0; i < T; i++) {
-      TreeMap<Integer, Integer> map = new TreeMap<>();
-      int k = scan.nextInt();
-
-      for (int j = 0; j < k; j++) {
-        String command = scan.next();
-        int num = scan.nextInt();
-        if (command.equals("I")) {
-          map.put(num, map.getOrDefault(num, 0) + 1);
-        } else if (command.equals("D")) {
-          if (map.isEmpty()) {
-            continue;
-          }
-          if (num == 1) {
-            Integer max = map.lastKey();
-            if (map.get(max) == 1) {
-              map.remove(max);
-            } else {
-              map.put(max, map.get(max) - 1);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        
+        for (int t = 0; t < T; t++) {
+            int k = Integer.parseInt(br.readLine());
+            PriorityQueue<Node> minHeap = new PriorityQueue<>();
+            PriorityQueue<Node> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+            boolean[] visited = new boolean[k];
+            
+            for (int i = 0; i < k; i++) {
+                String[] command = br.readLine().split(" ");
+                char op = command[0].charAt(0);
+                int num = Integer.parseInt(command[1]);
+                
+                if (op == 'I') {
+                    // 삽입
+                    Node node = new Node(num, i);
+                    minHeap.offer(node);
+                    maxHeap.offer(node);
+                    visited[i] = true;
+                } else if (op == 'D') {
+                    // 삭제
+                    if (num == 1) {
+                        // 최대값 삭제
+                        while (!maxHeap.isEmpty() && !visited[maxHeap.peek().index]) {
+                            maxHeap.poll();
+                        }
+                        if (!maxHeap.isEmpty()) {
+                            visited[maxHeap.poll().index] = false;
+                        }
+                    } else if (num == -1) {
+                        // 최소값 삭제
+                        while (!minHeap.isEmpty() && !visited[minHeap.peek().index]) {
+                            minHeap.poll();
+                        }
+                        if (!minHeap.isEmpty()) {
+                            visited[minHeap.poll().index] = false;
+                        }
+                    }
+                }
             }
-          } else {
-            Integer min = map.firstKey();
-            if (map.get(min) == 1) {
-              map.remove(min);
-            } else {
-              map.put(min, map.get(min) - 1);
+            
+            // 결과 출력
+            while (!minHeap.isEmpty() && !visited[minHeap.peek().index]) {
+                minHeap.poll();
             }
-          }
+            while (!maxHeap.isEmpty() && !visited[maxHeap.peek().index]) {
+                maxHeap.poll();
+            }
+            
+            if (minHeap.isEmpty() || maxHeap.isEmpty()) {
+                sb.append("EMPTY\n");
+            } else {
+                sb.append(maxHeap.peek().value).append(" ").append(minHeap.peek().value).append("\n");
+            }
         }
-      }
-
-      if (map.isEmpty()) {
-        sb.append("EMPTY").append("\n");
-      } else {
-        sb.append(map.lastKey()).append(" ").append(map.firstKey()).append("\n");
-      }
+        
+        System.out.print(sb.toString());
     }
 
-    System.out.println(sb);
-  }
-
-
-  static class FastReader {
-    BufferedReader br;
-    StringTokenizer st;
-
-    public FastReader() {
-      br = new BufferedReader(new InputStreamReader(System.in));
-    }
-
-    public FastReader(String s) throws FileNotFoundException {
-      br = new BufferedReader(new FileReader(new File(s)));
-    }
-
-    String next() {
-      while (st == null || !st.hasMoreElements()) {
-        try {
-          st = new StringTokenizer(br.readLine());
-        } catch (IOException e) {
-          e.printStackTrace();
+    // 우선순위 큐에 들어갈 노드 클래스
+    static class Node implements Comparable<Node> {
+        int value;
+        int index;
+        
+        Node(int value, int index) {
+            this.value = value;
+            this.index = index;
         }
-      }
-      return st.nextToken();
+        
+        @Override
+        public int compareTo(Node other) {
+            return Integer.compare(this.value, other.value);
+        }
     }
-
-    int nextInt() {
-      return Integer.parseInt(next());
-    }
-
-    long nextLong() {
-      return Long.parseLong(next());
-    }
-
-    double nextDouble() {
-      return Double.parseDouble(next());
-    }
-
-    String nextLine() {
-      String str = "";
-      try {
-        str = br.readLine();
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-      return str;
-    }
-  }
-
 }
