daa06b4012f5ba993e4585ffaf94d36ea4ad8c1b
Sungwon Choi <48509269+zhtmr@users.noreply.github.com>
2024-07-31 20:34:35 +0900

    [level 3] Title: 이중우선순위큐, Time: 54.18 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,46 +1,66 @@
 import java.util.*;
 
+ class Node implements Comparable<Node> {
+    int value;
+    int index;
+
+    Node(int value, int index) {
+      this.value = value;
+      this.index = index;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+      return Integer.compare(this.value, o.value);
+    }
+  }
+
 class Solution {
     public int[] solution(String[] operations) {
-        
-        TreeMap<Integer, Integer> map = new TreeMap<>();
+        PriorityQueue<Node> minHeap = new PriorityQueue<>();
+        PriorityQueue<Node> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+        boolean[] visited = new boolean[operations.length];
 
 
-        for (String command : operations) {
+        for (int i = 0, len = operations.length; i < len; i++) {
+          String command = operations[i];
           if (command.contains("I")) {
-            String num = command.split(" ")[1];
-            map.put(Integer.valueOf(num), map.getOrDefault(Integer.valueOf(num), 0) + 1);
+            int num = Integer.parseInt(command.split(" ")[1]);
+            Node node = new Node(num, i);
+            maxHeap.add(node);
+            minHeap.add(node);
+            visited[i] = true;
           } else if (command.equals("D 1")) {
-            if (map.isEmpty()) {
-              continue;
+            while (!maxHeap.isEmpty() && !visited[maxHeap.peek().index]) {
+              maxHeap.poll();
             }
-            Integer max = map.lastKey();
-            if (map.get(max) == 1) {
-              map.remove(max);
-            } else {
-              map.put(max, map.get(max) - 1);
+            if (!maxHeap.isEmpty()) {
+              visited[maxHeap.poll().index] = false;
             }
           } else {
-            if (map.isEmpty()) {
-              continue;
+            while (!minHeap.isEmpty() && !visited[minHeap.peek().index]) {
+              minHeap.poll();
             }
-            Integer min = map.firstKey();
-            if (map.get(min) == 1) {
-              map.remove(min);
-            } else {
-              map.put(min, map.get(min) - 1);
+            if (!minHeap.isEmpty()) {
+              visited[minHeap.poll().index] = false;
             }
           }
         }
-
+        
+        while (!minHeap.isEmpty() && !visited[minHeap.peek().index]) {
+          minHeap.poll();
+        }
+        while (!maxHeap.isEmpty() && !visited[maxHeap.peek().index]) {
+          maxHeap.poll();
+        }
 
         int[] result = new int[2];
-        if (map.isEmpty()) {
+        if (minHeap.isEmpty() || maxHeap.isEmpty()) {
           result[0] = 0;
           result[1] = 0;
         } else {
-          result[0] = map.lastKey();
-          result[1] = map.firstKey();
+          result[0] = maxHeap.peek().value;
+          result[1] = minHeap.peek().value;
         }
         return result;
     }
