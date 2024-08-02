4b692da344bad9b19b2f3a4ab960bb9b95b86960
Sungwon Choi <48509269+zhtmr@users.noreply.github.com>
2024-08-02 16:02:28 +0900

    Time: 7 ms (9.68%), Space: 41.5 MB (34.12%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int answer = 0;
+    Integer[] array = Arrays.stream(citations).boxed().toArray(Integer[]::new);
+    Arrays.sort(array, Collections.reverseOrder());
+
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] >= i + 1) {
+        answer = i + 1;
+      } else {
+        break;
+      }
+    }
+    return answer;
+    }
+}
