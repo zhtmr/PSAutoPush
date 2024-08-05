c3f974818c0d6e2bf91ab65181ee2133ed0bfbea
Sungwon Choi <48509269+zhtmr@users.noreply.github.com>
2024-08-05 15:17:12 +0900

    Time: 429 ms (34.45%), Space: 175.2 MB (5.26%) - LeetHub

--- 0745-prefix-and-suffix-search/0745-prefix-and-suffix-search.java
+++ 0745-prefix-and-suffix-search/0745-prefix-and-suffix-search.java
@@ -1,27 +1,50 @@
-class WordFilter {
-    private Map<String, Integer> map = new HashMap<>();
-    
-    public WordFilter(String[] words) {
-        for (int i = 0; i < words.length; i++) {
-          String word = words[i];
-          int length = word.length();
-          for (int j = 1; j <= length; j++) {
-            for (int k = 1; k <= length; k++) {
-              String key = word.substring(0, j) + "{" + word.substring(length - k);
-              map.put(key, i);
-            }
-          }
-        }
-    }
-    
-    public int f(String pref, String suff) {
-        String s = pref + "{" + suff;
-        return map.getOrDefault(s, -1);
+import java.util.HashMap;
+import java.util.Map;
+
+class TrieNode {
+    Map<Character, TrieNode> children;
+    int index;
+
+    public TrieNode() {
+        children = new HashMap<>();
+        index = -1;
     }
 }
 
-/**
- * Your WordFilter object will be instantiated and called as such:
- * WordFilter obj = new WordFilter(words);
- * int param_1 = obj.f(pref,suff);
- */
+class WordFilter {
+    private TrieNode root;
+
+    public WordFilter(String[] words) {
+        root = new TrieNode();
+        int n = words.length;
+        for (int i = 0; i < n; i++) {
+            String word = words[i];
+            int len = word.length();
+            // 모든 prefix와 suffix 조합을 저장
+            for (int j = 0; j <= len; j++) {
+                String modifiedWord = word.substring(j) + "{" + word;
+                insert(modifiedWord, i);
+            }
+        }
+    }
+
+    private void insert(String word, int index) {
+        TrieNode current = root;
+        for (char ch : word.toCharArray()) {
+            current = current.children.computeIfAbsent(ch, c -> new TrieNode());
+            current.index = index; // 현재 노드에 가장 큰 인덱스 저장
+        }
+    }
+
+    public int f(String prefix, String suffix) {
+        String searchWord = suffix + "{" + prefix;
+        TrieNode current = root;
+        for (char ch : searchWord.toCharArray()) {
+            current = current.children.get(ch);
+            if (current == null) {
+                return -1;
+            }
+        }
+        return current.index;
+    }
+}
