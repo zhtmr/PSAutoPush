31458f1bcf79bfb854d09b6bfbc5b4b35c0a74a7
Sungwon Choi <48509269+zhtmr@users.noreply.github.com>
2024-08-06 21:19:30 +0900

    [level 2] Title: 모음 사전, Time: 7.56 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,63 +1,37 @@
 import java.util.*;
 
 class Solution {
+    static String[] alphabet = {"A", "E", "I", "O", "U"};
+    static String[] all =
+        new String[(int) (5 + Math.pow(5, 2) + Math.pow(5, 3) + Math.pow(5, 4) + Math.pow(5, 5))];
+    static int index = 0;
+    
     public int solution(String word) {
         int answer = 0;
-        String[] alphabet = {"A", "E", "I", "O", "U"};
-
-        String[] all =
-            new String[(int) (5 + Math.pow(5, 2) + Math.pow(5, 3) + Math.pow(5, 4) + Math.pow(5, 5))];
-
-        int index = 0;
-
-        for (int i = 0; i < 5; i++) {
-          all[index++] = alphabet[i];
-        }
-
-        for (int i = 0; i < 5; i++) {
-          for (int j = 0; j < 5; j++) {
-            all[index++] = alphabet[i] + alphabet[j];
-          }
-        }
-
-        for (int i = 0; i < 5; i++) {
-          for (int j = 0; j < 5; j++) {
-            for (int k = 0; k < 5; k++) {
-              all[index++] = alphabet[i] + alphabet[j] + alphabet[k];
-            }
-          }
-        }
-
-        for (int i = 0; i < 5; i++) {
-          for (int j = 0; j < 5; j++) {
-            for (int k = 0; k < 5; k++) {
-              for (int l = 0; l < 5; l++) {
-                all[index++] = alphabet[i] + alphabet[j] + alphabet[k] + alphabet[l];
-              }
-            }
-          }
-        }
-
-        for (int i = 0; i < 5; i++) {
-          for (int j = 0; j < 5; j++) {
-            for (int k = 0; k < 5; k++) {
-              for (int l = 0; l < 5; l++) {
-                for (int m = 0; m < 5; m++) {
-                  all[index++] = alphabet[i] + alphabet[j] + alphabet[k] + alphabet[l] + alphabet[m];
-                }
-              }
-            }
-          }
+        
+        
+        for (int i = 1; i <= 5; i++) {
+            dfs("", i, 0);
         }
         
         Arrays.sort(all);
         for (int i = 0; i < all.length; i++) {
             if (all[i].equals(word)) {
                 answer = i + 1;
-                break;
             }
         }
         
+        
         return answer;
+    }
+    
+    static void dfs(String word, int length, int depth) {
+        if (depth == length) {
+            all[index++] = word;
+            return;
+        }
+        for (String a : alphabet) {
+            dfs(word + a, length, depth + 1);
+        }
     }
 }
