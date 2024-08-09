065ff8705bd9a4b14c1445a8df7d13ff68b34b5e
Sungwon Choi <48509269+zhtmr@users.noreply.github.com>
2024-08-09 16:26:02 +0900

    [level 2] Title: 구명보트, Time: 11.29 ms, Memory: 55.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+         Arrays.sort(people);
+    int left = 0;
+    int right = people.length - 1;
+
+    while (left <= right) {
+      if (people[left] + people[right] <= limit) {
+        left++;
+      }
+      answer++;
+      right--;
+    }
+        return answer;
+    }
+}
