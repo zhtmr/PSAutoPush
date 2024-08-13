22439e9120ab281cc7b6e906e83500fa61f239bb
Sungwon Choi <48509269+zhtmr@users.noreply.github.com>
2024-08-14 02:05:05 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.10 ms, Memory: 67.5 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -1,16 +1,19 @@
 class Solution {
     public int solution(int storey) {
-        int answer = 0;
-        while (storey > 0) {
-          int lastNum = storey % 10;
-          if (lastNum > 5 || (lastNum == 5 && (storey / 10) % 10 >= 5)) {
-            answer += (10 - lastNum);
-            storey += (10 - lastNum);
-          } else {
-            answer += lastNum;
-          }
-          storey /= 10;
+        int answer;
+        if (storey <= 5) {
+          return storey;
+        } else if (storey < 10) {
+          return 11 - storey;
         }
+
+        int lastNum = storey % 10;
+        int down = solution(storey / 10) + lastNum;
+        int up = solution(storey / 10 + 1) + (10 - lastNum);
+        answer = Math.min(down, up);
+
         return answer;
+        
+        
     }
 }
