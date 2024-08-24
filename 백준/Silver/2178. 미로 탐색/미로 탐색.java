20f98058e220a7597fcfc1a75e4258b1240bf2ae
Sungwon Choi <48509269+zhtmr@users.noreply.github.com>
2024-08-25 04:18:48 +0900

    [Silver I] Title: 미로 탐색, Time: 156 ms, Memory: 16572 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+  static int[][] graph;
+  static boolean[][] visited;
+  static int N, M;
+  static int[] dx = {-1, 1, 0, 0};
+  static int[] dy = {0, 0, -1, 1};
+  static int count;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+
+    graph = new int[N + 2][M + 2];
+    visited = new boolean[N + 2][M + 2];
+
+    for (int i = 1; i <= N; i++) {
+      String[] split = br.readLine().split("");
+      for (int j = 1; j <= M; j++) {
+        graph[i][j] = Integer.parseInt(split[j - 1]);
+      }
+    }
+
+//    count = 1;
+    bfs(1, 1);
+    System.out.println(count);
+  }
+
+  private static void bfs(int x, int y) {
+    Queue<int[]> queue = new LinkedList<>();
+    queue.add(new int[] {x, y, 1});
+    visited[x][y] = true;
+
+    while (!queue.isEmpty()) {
+      int[] current = queue.poll();
+      int cx = current[0];
+      int cy = current[1];
+      int depth = current[2];
+
+      if (cx == N && cy == M) {
+        count = depth;
+        return;
+      }
+
+      for (int i = 0; i < 4; i++) {
+        int nx = cx + dx[i];
+        int ny = cy + dy[i];
+
+        if (!visited[nx][ny] && graph[nx][ny] != 0) {
+          queue.add(new int[] {nx, ny, depth + 1});
+          visited[nx][ny] = true;
+        }
+      }
+    }
+
+  }
+}
