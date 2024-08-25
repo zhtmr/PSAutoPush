889e8eb0cf2aac1dd49d8f919dac1340940ffcae
Sungwon Choi <48509269+zhtmr@users.noreply.github.com>
2024-08-25 17:52:08 +0900

    [Silver III] Title: 1로 만들기, Time: 104 ms, Memory: 15176 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,49 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
-import java.util.Queue;
 
 public class Main {
-    
-    public static void main(String[] args) {
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
-        int n = scanner.nextInt();
-        scanner.close();
-        
-        System.out.println(bfs(n));
+  static boolean[] visited;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    visited = new boolean[N + 1];
+    System.out.println(bfs(N));
+  }
+
+  private static int bfs(int num) {
+    LinkedList<int[]> queue = new LinkedList<>();
+    queue.add(new int[] {num, 0});
+    visited[num] = true;
+
+    while (!queue.isEmpty()) {
+      int[] current = queue.poll();
+      int n = current[0];
+      int count = current[1];
+
+      if (n == 1) {
+        return count;
+      }
+
+      if (n % 3 == 0 && !visited[n / 3]) {
+        int q = n / 3;
+        queue.add(new int[] {q, count + 1});
+        visited[q] = true;
+      }
+      if (n % 2 == 0 && !visited[n / 2]) {
+        int q = n / 2;
+        queue.add(new int[] {q, count + 1});
+        visited[q] = true;
+      }
+      if (!visited[n - 1]) {
+        queue.add(new int[] {n - 1, count + 1});
+        visited[n - 1] = true;
+      }
     }
-
-    private static int bfs(int n) {
-        Queue<int[]> queue = new LinkedList<>();
-        boolean[] visited = new boolean[n + 1];
-
-        queue.add(new int[] { n, 0 });
-        visited[n] = true;
-
-        while (!queue.isEmpty()) {
-            int[] current = queue.poll();
-            int number = current[0];
-            int count = current[1];
-
-            if (number == 1) {
-                return count;
-            }
-
-            
-            if (number % 3 == 0 && !visited[number / 3]) {
-                queue.add(new int[] { number / 3, count + 1 });
-                visited[number / 3] = true;
-            }
-
-            if (number % 2 == 0 && !visited[number / 2]) {
-                queue.add(new int[] { number / 2, count + 1 });
-                visited[number / 2] = true;
-            }
-
-            if (!visited[number - 1]) {
-                queue.add(new int[] { number - 1, count + 1 });
-                visited[number - 1] = true;
-            }
-        }
-
-        return -1; 
-    }
+    return -1;
+  }
 }
