218436b7e9a909eb547456d1dbd73d5005cb7c34
Sungwon Choi <48509269+zhtmr@users.noreply.github.com>
2024-09-02 00:19:50 +0900

    Time: 5 ms (97.30%), Space: 56.7 MB (94.59%) - LeetHub

--- 1997-first-day-where-you-have-been-in-all-the-rooms/1997-first-day-where-you-have-been-in-all-the-rooms.java
+++ 1997-first-day-where-you-have-been-in-all-the-rooms/1997-first-day-where-you-have-been-in-all-the-rooms.java
@@ -1,16 +1,16 @@
 class Solution {
     public int firstDayBeenInAllRooms(int[] nextVisit) {
         int n = nextVisit.length;
-        int[] dp = new int[n];
+        long[] dp = new long[n];
         int mod = (int) 1e9 + 7;
 
         dp[0] = 0;  // 시작 방이기 때문에 첫날은 0
 
         for (int i = 1; i < n; i++) {
             // dp[i] = 이전 방까지 걸린 시간 + 현재 방에 도착하는 시간 + 다시 돌아가는 시간
-            dp[i] = (dp[i - 1] + 1 + (dp[i - 1] - dp[nextVisit[i - 1]] + mod) % mod + 1) % mod;
+            dp[i] = (dp[i-1] * 2 - dp[nextVisit[i-1]] + 2 + mod) % mod;
         }
 
-        return dp[n - 1];
+        return (int) dp[n - 1];
     }
 }
